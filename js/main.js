b3e0c6f1144979017835d566db808d3e7ef3bef9
tianjihai <jihai123@vip.qq.com>
2018-08-10 22:52:01 +0800

    轮播图

--- js/main.js
+++ js/main.js
@@ -3,6 +3,45 @@ $(document).ready(function () {
         $(".cartMain").stop().slideDown(200);
     }).mouseout(function () {
         $(".cartMain").stop().slideUp(200);
-    })
+    });
+
+
+
+
+    var $li = $(".slider>ul>li");
+    var count = 0;
+
+    //默认自动轮播下一张图片
+    function lunbo() {
+        count++;
+        if(count === $li.length){
+            count = 0;
+        }
+        $li.eq(count).fadeIn(400).siblings().fadeOut(400);
+    }
+    var lb=setInterval(lunbo,1500);
+
+
+    //点击下一张切换到下一张图片然后停止自动轮播定时器
+    $(".arrow-right").click(function () {
+        count++;
+        if(count === $li.length){
+            count = 0;
+        }
+        $li.eq(count).fadeIn(400).siblings().fadeOut(400);
+        clearInterval(lb);
+    });
+
+    //点击上一张切换上一张图片然后停止自动轮播定时器
+    $(".arrow-left").click(function () {
+        count--;
+        if(count === -1){
+            count = $li.length - 1;
+        }
+        $li.eq(count).fadeIn(400).siblings().fadeOut(400);
+        clearInterval(lb);
+    });
+
+
 
 });
